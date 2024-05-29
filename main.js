b9a0ad2bdc3e2f0584be1e7a7795ccfbd48ab976
LvSira <gomezvalentin541@gmail.com>
2024-05-29 17:21:25 -0300

    texto

---
+++ main.js
@@ -0,0 +1,28 @@
+let DiaSem = prompt ("Ingrese el dia de la semana: ")
+
+switch(DiaSem){
+    case "lunes": console.log("laboral");
+    alert ("El dia es: laboral! :(");
+    break;
+    case "martes": console.log("laboral");
+    alert ("El dia es: laboral! :(");
+    break;
+    case "miercoles": console.log("laboral");
+    alert ("El dia es: laboral! :(");
+    break;
+    case "jueves": console.log("laboral");
+    alert ("El dia es: laboral! :(");
+    break;
+    case "viernes": console.log("laboral");
+    alert ("El dia es: laboral! :(");
+    break;
+    case "sabado": console.log("libre");
+    alert ("El dia es: libre!");
+    break;
+    case "domingo": console.log("libre");
+    alert ("El dia es: libre!");
+    break;
+    default: console.log("dia invalido");
+    alert ("El dia ingresado no es valido");
+}
+
